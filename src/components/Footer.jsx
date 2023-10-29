39ffef085bff607941c1edbb224eda6b6156d1f5
✧ Victor ✧ <vkrenzel@outlook.com>
2023-10-29 12:18:00 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,13 +12,13 @@ const Footer = () => {
                 </div>
             </div>
             <div className="flex flex-col md:flex-row gap-3 w-full">
-                <div className=" bg-white p-5 rounded-2xl lg:rounded-full w-full flex-col md:flex-row md:justify-between">
+                <div className="bg-white p-5 rounded-2xl lg:rounded-full w-full flex flex-col md:flex-row md:justify-between">
                     <div className="md:ms-4 text-xs lg:text-sm flex gap-1 items-center justify-center md:justify-start">
                         <p>Made with</p> 
                         <PiHeartDuotone className="w-5 h-5 text-rose-300" />
                         <p>by Victor Krenzel</p>
                     </div>
-                    <hr className="w-24 mx-auto my-3 md:hidden"/>
+                    <hr className="w-24 mx-auto my-3 md:hidden" />
                     <div className="text-xs lg:text-sm sm:ms-4 md:me-4 text-center md:text-left mt-1">
                         vkrenzel &copy; 2023
                     </div>
