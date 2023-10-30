3e009007127735d218e3971b0a414b058e4c9fba
✧ Victor ✧ <vkrenzel@outlook.com>
2023-10-30 00:55:30 -0400

    Slower `body` transition speed

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { PiLightbulbFilamentDuotone, PiMoonStarsDuotone } from 'react-icons/pi'
+import { PiMoonStarsDuotone } from 'react-icons/pi'
 import { useLocation } from 'react-router-dom'
 import { Navbar } from 'flowbite-react';
 
