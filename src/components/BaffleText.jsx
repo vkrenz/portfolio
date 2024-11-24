682ba593d90aecc1c851d24f946e6c633ffe48c8
✧ Victor ✧ <vkrenzel@outlook.com>
2024-11-23 21:43:21 -0500

    Clean Up BaffleJS Code

--- src/components/BaffleText.jsx
+++ src/components/BaffleText.jsx
@@ -1,13 +1,20 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
 
+// React Imports
 import { useEffect, useRef } from 'react';
+
+// Baffle Js Import
 import baffle from 'baffle';
 
 const BaffleText = ({ text }) => {
   const baffleRef = useRef(null);
 
-  const disableBaffle = true
+  // Main Baffle JS Control
+  const disableBaffle = false
+  const speed = Math.random() * (200 - 50) + 50
+  const delay = Math.random() * (100 - 10) + 10
+  const duration = 2000
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -16,11 +23,11 @@ const BaffleText = ({ text }) => {
           if (entry.isIntersecting) {
             const target = baffle(baffleRef.current);
             target.set({
-              characters:
-                "経視常受気武会軽愛員口賞兆高笑準運府冬遠時読科康和換針均効意持融散国欧備屋茶数話>",
-                speed: Math.random() * (200 - 50) + 50,
-                delay: Math.random() * (100 - 10) + 10,
-                duration: 2000,
+              // characters:
+              //   "経視常受気武会軽愛員口賞兆高笑準運府冬遠時読科康和換針均効意持融散国欧備屋茶数話>",
+                speed: speed,
+                delay: delay,
+                duration: duration,
             });
             target.start();
             target.reveal(1000, 100);
