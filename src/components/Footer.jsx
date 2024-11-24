7993f8f8941694afad36ed0e62627a6440a0fc1f
✧ Victor ✧ <vkrenzel@outlook.com>
2024-11-23 21:53:29 -0500

    Footer Code Cleanup Pt 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,13 +19,15 @@ const Footer = () => {
     return (
         <div className="flex flex-col w-full">
             <div className="flex gap-3 my-14 dark-text-color max-h-[60px]">
-                <div
-                    className={`${isDarkMode ? 'bg-neutral-600' : 'bg-white'} expand-hover cursor-pointer rounded-2xl lg:rounded-full md:flex hidden items-center justify-center px-6 w-1/5`}>
+
+                {/* Source Code Button (Navigates to Portfolio Github Repo) @ Large Screen */}
+                <div className={`${isDarkMode ? 'bg-neutral-600' : 'bg-white'} expand-hover cursor-pointer rounded-2xl lg:rounded-full md:flex hidden items-center justify-center px-6 w-1/5`}>
                     <a href="https://github.com/vkrenz/portfolio-2" className={`${isDarkMode ? 'text-white' : 'dark-text-color'} flex gap-3 items-center text-xs lg:text-md`}>
                         <FaGithubAlt className="w-5 h-5" />
                         <BaffleText text="Source Code"/>
                     </a>
                 </div>
+
                 <div className="flex flex-col md:flex-row gap-3 w-full">
                     <div className={`${isDarkMode ? 'bg-neutral-600' : 'bg-white'} p-5 rounded-2xl lg:rounded-full w-full flex flex-col md:flex-row md:justify-between`}>
                         <div className={`${isDarkMode ? 'light-text-color' : 'dark-text-color'} md:ms-4 text-xs lg:text-sm flex gap-1 items-center justify-center md:justify-start`}>
@@ -38,6 +40,8 @@ const Footer = () => {
                             vkrenzel &copy; 2023-2024
                         </div>
                     </div>
+
+                    {/* Source Code Button (Navigates to Portfolio Github Repo) @ Small Screen */}
                     <div className={`${isDarkMode ? 'bg-neutral-600' : 'bg-white'} expand-hover cursor-pointer rounded-2xl lg:rounded-full flex md:hidden items-center justify-center p-6`}>
                         <a href="https://github.com/vkrenz/portfolio-2" className={`${isDarkMode ? 'text-white' : 'dark-text-color'} flex gap-3 items-center text-xs lg:text-md`}>
                             <FaGithubAlt className="w-5 h-5" />
@@ -45,7 +49,9 @@ const Footer = () => {
                             <FiArrowUpRight className="w-5 h-5" />
                         </a>
                     </div>
+
                 </div>
+
             </div>
         </div>
     );
